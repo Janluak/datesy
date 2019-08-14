130d7e972d4733f032af97ac39b08da334cd9a26
janluak <janluak@users.noreply.github.com>
2019-08-14 17:21:06 +0200

    single file load support

--- aybasics/files/load.py
+++ aybasics/files/load.py
@@ -3,6 +3,8 @@
 def load_json(files):
     from json import load
     data = dict()
+    if not isinstance(files, list):
+        files = [files]
     for file in files:
         with open(file, "r") as f:
             data[file] = load(f)
@@ -16,6 +18,8 @@ def load_json(files):
 def load_csv(files, dialect=None):
     from csv import reader
     data = dict()
+    if not isinstance(files, list):
+        files = [files]
     for file in files:
         with open(file, "r") as f:
             data[file] = list()
