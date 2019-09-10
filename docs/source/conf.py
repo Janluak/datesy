fc62a33e7023fd0f143f01dfa2eaf2c8995c2a71
janluak <janluak@users.noreply.github.com>
2019-09-10 11:48:23 +0200

    autodoc mock

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../datesy'))
+autodoc_mock_imports = ["datesy"]
 
 
 # -- Project information -----------------------------------------------------
