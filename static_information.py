7b136fc91dfb4584f25d7ba1e03fa4b26dabcd28
janluak <Szh-wcK-Efg-y3G>
2019-12-18 16:05:04 +0100

    bug fixes -> 0.6.16.post2

--- static_information.py
+++ static_information.py
@@ -1,6 +1,6 @@
 # static information that is relevant for packet management and documentation
 project_name = "datesy"
-version = "0.6.16post1"
+version = "0.6.16post2"
 url = "https://github.com/janluak/datesy"
 author = "Jan Lukas Braje"
 author_email = "datesy@braje.org"
