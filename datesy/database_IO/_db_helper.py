7f37ddd535922f2630fe72a8a3d10be324a6393e
janluak <Szh-wcK-Efg-y3G>
2020-01-09 13:57:57 +0100

    warning: database still in development

--- datesy/database_IO/_db_helper.py
+++ datesy/database_IO/_db_helper.py
@@ -413,6 +413,9 @@ class Database:
         specific information to database, see details to each database
 
     """
+    import warnings
+    warnings.warn("\n\nDatabase interface still in development. Changes may apply\n", UserWarning)
+
     def __init__(self, host, port, user, password, database, auto_creation=False):
         self._host = host
         self._port = port
