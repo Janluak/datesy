ac661cabb87b48fe6699cc43e3b0d2fc59228c4f
janluak <Szh-wcK-Efg-y3G>
2020-01-23 14:23:00 +0100

    v0.7.1

--- static_information.py
+++ static_information.py
@@ -1,6 +1,6 @@
 # static information that is relevant for packet management and documentation
 project_name = "datesy"
-version = "0.7.0"
+version = "0.7.1"
 url = "https://github.com/janluak/datesy"
 author = "Jan Lukas Braje"
 author_email = "datesy@braje.org"
